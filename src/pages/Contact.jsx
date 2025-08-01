fc4cb526b73a9959a95e0bb4571dbddb9aace577
Paul <alvercapaul@gmail.com>
2025-07-31 22:48:48 -0500

    add some data

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+  return (
+    <div>
+      <h1>Contacto</h1>
+      <p>Puedes contactarnos a través de este formulario.</p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Página de Inicio</h1>
+      <p>Bienvenido a nuestro sitio web.</p>
+    </div>
+  );
+}
+
+export default Home;
